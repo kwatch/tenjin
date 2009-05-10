5e3919010de2a4cb01ecfd52639d4571d99e65c8
makoto kuwata <kwa@kuwata-lab.com>
2009-05-10 22:39:37 +0900

    python: remove 'test/Kookbook.yaml' and add 'test/Kookbook.py' instead

---
+++ python/test/Kookbook.py
@@ -0,0 +1,28 @@
+srcdir = "../../../rbtenjin/trunk/test"
+kook_default_product = 'test'
+
+
+@ingreds('test_engine.yaml', 'test_template.yaml')
+def task_copy(c):
+    """copy data file (*.yaml)"""
+    pass
+
+
+@product('test_*.yaml')
+@ingreds(srcdir + '/test_$(1).yaml')
+def file_test_engine_yaml(c):
+    cp_p(c.ingred, c.product)
+
+
+@ingreds('copy')
+@byprods('test.log')
+@cmdopts('-v: verbose')
+def task_test(c, *args):
+    options, rest = c.parse_cmdopts(args)
+    opts = options.get('v') and ' -v ' or ''
+    system(c%'python test_all.py $(opts) 2>&1 | tee $(byprod)')
+    #system(c%'python test_all.py $(opts) 2>&1 > $(byprod)')
+
+
+def task_clean(c):
+    rm_f('test.log', '*.pyc', '**/*.pyc', '**/*.cache')
