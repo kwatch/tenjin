bf2254489c591b13584a57f2d15e837195f9aaa7
makoto kuwata <kwa@kuwata-lab.com>
2009-05-20 04:28:49 +0900

    python: change 'test/Kookbook.py' not to invoke 'test_{users_guide,faq,examples}.py' when Python 3.0

--- python/test/Kookbook.py
+++ python/test/Kookbook.py
@@ -1,6 +1,10 @@
 srcdir = "../../common/test"
 kook_default_product = 'test'
 
+import sys
+python3 = sys.version_info[0] == 3
+python2 = sys.version_info[0] == 2
+
 
 @ingreds('test_engine.yaml', 'test_template.yaml')
 def task_copy(c):
@@ -20,9 +24,16 @@ def file_test_engine_yaml(c):
 def task_test(c, *args):
     options, rest = c.parse_cmdopts(args)
     opts = options.get('v') and ' -v ' or ''
-    #system(c%'python test_all.py $(opts) 2>&1 | tee $(byprod)')
-    system(c%'python test_all.py $(opts)')
-    #system(c%'python test_all.py $(opts) 2>&1 > $(byprod)')
+    if python2:
+        #system(c%'python test_all.py $(opts) 2>&1 | tee $(byprod)')
+        system(c%'python test_all.py $(opts)')
+        #system(c%'python test_all.py $(opts) 2>&1 > $(byprod)')
+    elif python3:
+        system(c%'python test_template.py $(opts)')
+        system(c%'python test_engine.py $(opts)')
+        system(c%'python test_main.py $(opts)')
+        system(c%'python test_htmlhelper.py $(opts)')
+        system(c%'python test_preprocess.py $(opts)')
     task_clean(c)
 
 
