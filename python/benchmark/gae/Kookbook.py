82521d3c60ceab5135e61e2d931cb5e94dcf6dda
makoto kuwata <kwa@kuwata-lab.com>
2011-02-16 13:14:49 +0900

    python: refactor 'benchmark/gae/Kookbook.py'

--- python/benchmark/gae/Kookbook.py
+++ python/benchmark/gae/Kookbook.py
@@ -29,45 +29,41 @@ def file_tenjin_py(c):
     cp_p(c.ingred, c.product)
 
 
+def convert_template(c, func):
+    with open(c.ingred, 'rb') as f:
+        s = f.read()
+    s = func(s)
+    with open(c.product, 'wb') as f:
+        f.write(s)
+
+
 @recipe
 @product('templates/bench_django.html')
 @ingreds('templates/escape_django.html')
 def file_bench_django_html(c):
-    with open(c.ingred, 'rb') as f:
-        s = f.read()
-    s = s.replace('|escape', '').replace('escape_django', 'bench_django')
-    with open(c.product, 'wb') as f:
-        f.write(s)
+    f = lambda s: s.replace('|escape', '').replace('escape_django', 'bench_django')
+    convert_template(c, f)
 
 
 @recipe
 @product('templates/bench_django12.html')
 @ingreds('templates/escape_django.html')
 def file_bench_django12_html(c):
-    with open(c.ingred, 'rb') as f:
-        s = f.read()
-    s = s.replace('|escape', '|safe').replace('escape_django', 'bench_django12')
-    with open(c.product, 'wb') as f:
-        f.write(s)
+    f = lambda s: s.replace('|escape', '|safe').replace('escape_django', 'bench_django12')
+    convert_template(c, f)
 
 
 @recipe
 @product('templates/escape_django12.html')
 @ingreds('templates/escape_django.html')
 def file_escape_django12_html(c):
-    with open(c.ingred, 'rb') as f:
-        s = f.read()
-    s = s.replace('|escape', '').replace('escape_django', 'escape_django12')
-    with open(c.product, 'wb') as f:
-        f.write(s)
+    f = lambda s: s.replace('|escape', '').replace('escape_django', 'escape_django12')
+    convert_template(c, f)
 
 
 @recipe
 @product('templates/bench_tenjin.pyhtml')
 @ingreds('templates/escape_tenjin.pyhtml')
 def file_escape_tenjin_pyhtml(c):
-    with open(c.ingred, 'rb') as f:
-        s = f.read()
-    s = s.replace('${', '#{').replace('escape_tenjin', 'bench_tenjin')
-    with open(c.product, 'wb') as f:
-        f.write(s)
+    f = lambda s: s.replace('${', '#{').replace('escape_tenjin', 'bench_tenjin')
+    convert_template(c, f)
